97bbf8a8c8aeb2dd8bef7ca5578b0066e4f3294d
Алания Илья <alaniyailya@mail.ru>
2022-08-01 20:01:25 +0300

    first commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,7 +10,6 @@ public class Main {
         post.surname = "Иванов";
         post.subscription = true;
 
-
         post.birthday = new FormDate();
         post.birthday.day = 13;
         post.birthday.month = 6;
