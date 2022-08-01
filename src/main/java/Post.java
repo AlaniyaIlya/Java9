9917ac34d4813edc18240ce4ceae9295fc165707
Алания Илья <alaniyailya@mail.ru>
2022-08-01 19:54:21 +0300

    first commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,15 @@
+import java.net.PortUnreachableException;
+import java.security.PublicKey;
+
+public class Post {
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public boolean subscription;
+
+    public FormDate birthday;
+    public int year;
+    public int month;
+}
